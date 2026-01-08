dfbb1acbd8ae6786e03134bea01231288c4a5c44
Bahar Işıklı <baharisikli@outlook.com>
2026-01-08 03:46:11 +0300

    feed: implement session management and routing

--- php/feed.php
+++ php/feed.php
@@ -1,3 +1,39 @@
+<?php 
+    session_start();
+    //db bağlantı
+    $host = 'localhost'; $db = 'proje'; $user = 'root'; $pass = ''; $charset = 'utf8mb4';
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    try {
+        $pdo = new PDO($dsn, $user, $pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    } catch (\PDOException $e) {
+        die("Bağlantı hatası: " . $e->getMessage());
+    }
+
+    //login 
+    if (!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== true) {
+    header("Location: ../login.html"); 
+    exit();
+    }
+
+    //değişkenleri alma
+    $username = isset($_SESSION['username']) ? htmlspecialchars($_SESSION['username']) : 'Misafir';
+    $user_id = $_SESSION['user_id']; 
+
+    //logout butonu
+    if(isset($_POST['logout-button'])){
+      session_destroy();
+      header("Location: ../login.html"); 
+    }
+
+    //profil butonu
+    if (isset($_POST['profile-button'])) {
+    header("Location: mainpage.php");
+    exit();
+    }
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +48,7 @@
         <div class="svg-container">
           <img src="../assets/logo.svg" alt="user-icon" class="sidebar-svg" />
         </div>
-        <p>username</p>
+        <p><?php echo $username; ?></p>
       </div>
       <div class="side-bar-content">
         <form method="POST" action = "">
